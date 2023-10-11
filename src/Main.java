cc90cfacbc44ad90ac117527394c8dea914d974c
sawae <$$Kacperus16>
2023-10-11 21:15:49 +0200

    Simple version

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double value_1 = 10.0d;
+        double value_2 = 5.0d;
+        double result = 0.0d;
+        char equation = 'd';
+
+        if(equation == 'a')
+            result = value_1 + value_2;
+        else if (equation == 'm')
+            result = value_1 * value_2;
+        else if (equation == 's')
+            result = value_1 - value_2;
+        else if (equation == 'd')
+            if (value_2 != 0)
+                result = value_1 / value_2;
+        else {
+            System.out.println("Something went wrong.");
+            result = 0.0d;
+        }
+        System.out.println(result);
     }
 }
